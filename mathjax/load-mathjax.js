b972fbd72487b78bfe6ab228fb61d4aec53874bc
William Gertler <will.gertler@gmail.com>
2019-12-29 16:56:37 -0500

    help

--- mathjax/load-mathjax.js
+++ mathjax/load-mathjax.js
@@ -7,7 +7,7 @@ window.MathJax = {
 
 (function () {
     var script = document.createElement('script');
-    script.src = 'https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML';
+    script.src = 'https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS_HTML';
     script.async = true;
     document.head.appendChild(script);
 })();
