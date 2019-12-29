538f137f94cd185d87cb1a650368a32793d936b0
William Gertler <will.gertler@gmail.com>
2019-12-29 16:35:03 -0500

    help

--- mathjax/load-mathjax.js
+++ mathjax/load-mathjax.js
@@ -1,13 +1,16 @@
 window.MathJax = {
+    loader: {load: ['[tex]/braket']},
+    tex: {packages: {'[+]': ['braket']}},
     tex2jax: {
         inlineMath: [['$', '$'], ['$$', '$$']],
         processEscapes: true
-        }
-    };
+    },
+};
 
 (function () {
     var script = document.createElement('script');
     script.src = 'https://polyfill.io/v3/polyfill.min.js?features=es6'
+    script.src = 'https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML';
     script.async = true;
     document.head.appendChild(script);
 })();
