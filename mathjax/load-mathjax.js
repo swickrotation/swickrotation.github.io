54fca762e4a68c741c4f9f1b8a76b9e4e0b114e6
William Gertler <will.gertler@gmail.com>
2019-12-29 15:58:34 -0500

    help

--- mathjax/load-mathjax.js
+++ mathjax/load-mathjax.js
@@ -10,7 +10,6 @@ window.MathJax = {
 (function () {
     var script = document.createElement('script');
     script.src = 'https://polyfill.io/v3/polyfill.min.js?features=es6'
-    script.src = 'https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML';
     script.async = true;
     document.head.appendChild(script);
 })();
