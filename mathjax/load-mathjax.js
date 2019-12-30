90959ecd17d6b643159c2a99af149b2511a76838
William Gertler <will.gertler@gmail.com>
2019-12-29 19:24:51 -0500

    help

--- mathjax/load-mathjax.js
+++ mathjax/load-mathjax.js
@@ -1,12 +1,3 @@
-window.MathJax = {
-    loader: {load: ['[tex]/amscd']},
-    tex: {packages: {'[+]': ['amscd']}},
-    tex2jax: {
-        inlineMath: [['$', '$'], ['$$', '$$']],
-        processEscapes: true
-    }
-};
-
 (function () {
     var script = document.createElement('script');
     script.src = 'https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS_HTML';
