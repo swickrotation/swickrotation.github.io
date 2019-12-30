b7ff181f47a7431c86c6e8e325da0868a0f700d4
William Gertler <will.gertler@gmail.com>
2019-12-29 20:24:44 -0500

    help

--- mathjax/load-mathjax.js
+++ mathjax/load-mathjax.js
@@ -1,13 +1,4 @@
-window.MathJax = {
-    tex2jax: {
-        inlineMath: [['$', '$'], ['$$', '$$']],
-        processEscapes: true
-    }
-};
-
-(function () {
-    var script = document.createElement('script');
-    script.src= "https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js";
-    script.async = true;
-    document.head.appendChild(script);
-})();
+<script src="/mathjax/mathjax-config.js" defer></script>
+<script type="text/javascript" id="MathJax-script" defer
+    src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js ">
+</script>
